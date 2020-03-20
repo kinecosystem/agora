eb655af1e468c7cfbe2e42bfc7b01b33c867b626
Kik Engineering <engineering@kik.com>
2020-03-20 19:01:25 -0400

    cmd/ledgerstats: properly page txns, print tx + op counts
    GitOrigin-RevId: 619295c34f7e4ae8d3ae6cf42ed03decffccadc6

--- cmd/ledgerstats/main.go
+++ cmd/ledgerstats/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/kinecosystem/agora-common/kin"
 	"github.com/stellar/go/clients/horizonclient"
+	hProtocol "github.com/stellar/go/protocols/horizon"
 )
 
 func run() error {
@@ -23,29 +24,41 @@ func run() error {
 	}
 
 	for _, r := range page.Embedded.Records {
-		counts := make(map[string]int)
+		opCount := make(map[string]int)
+		txCount := make(map[string]int)
 
-		txns, err := client.Transactions(horizonclient.TransactionRequest{
+		var txnPage hProtocol.TransactionsPage
+		txnPage, err = client.Transactions(horizonclient.TransactionRequest{
 			ForLedger: uint(r.Sequence),
+			Limit:     100,
 		})
 		if err != nil {
 			return err
 		}
 
-		for _, t := range txns.Embedded.Records {
-			var k string
-			if t.MemoType != "text" {
-				k = "other"
-			} else {
-				k = t.Memo[:5]
+		for len(txnPage.Embedded.Records) > 0 {
+			for _, t := range txnPage.Embedded.Records {
+				var k string
+				if t.MemoType != "text" {
+					k = "other"
+				} else {
+					k = t.Memo[:5]
+				}
+
+				opCount[k] += int(t.OperationCount)
+				txCount[k]++
 			}
 
-			counts[k] += int(t.OperationCount)
+			txnPage, err = client.NextTransactionsPage(txnPage)
+			if err != nil {
+				fmt.Println("Failed to get complete results")
+				break
+			}
 		}
 
-		fmt.Printf("Ledger: %d\n---\n", r.Sequence)
-		for k, v := range counts {
-			fmt.Printf("%s:\t%d\n", k, v)
+		fmt.Printf("Ledger (key, tx, op):%d\n---\n", r.Sequence)
+		for k, v := range opCount {
+			fmt.Printf("%s:\t%d\t%d\n", k, txCount[k], v)
 		}
 		fmt.Println("")
 	}
